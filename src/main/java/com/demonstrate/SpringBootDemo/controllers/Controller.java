fb53ba586023330663797c1e72cd66b05e558ee6
ToodlyDave <alapatdavid@gmail.com>
2022-01-21 20:46:48 +0530

    CREATE a method to handle rest api calls that accepts path variables

--- src/main/java/com/demonstrate/SpringBootDemo/controllers/Controller.java
+++ src/main/java/com/demonstrate/SpringBootDemo/controllers/Controller.java
@@ -1,6 +1,7 @@
 package com.demonstrate.SpringBootDemo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +19,10 @@ public class Controller {
 	public String sayHello(@RequestParam(value = "name") String name) {
 		return "Hello " + name + " from Bridgelabz";
 	}
+	
+	@GetMapping("/param/{name}")
+	public String sayHelloPath(@PathVariable String name) {
+		return "Hello " + name + " from Bridgelabz";
+	}
+	
 }
