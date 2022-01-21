7ceec57ca7c19df349169c52e3141bad85551db6
ToodlyDave <alapatdavid@gmail.com>
2022-01-21 21:09:21 +0530

    CREATE method to handle post rest api calls that accepts json parameter

--- src/main/java/com/demonstrate/SpringBootDemo/controllers/Controller.java
+++ src/main/java/com/demonstrate/SpringBootDemo/controllers/Controller.java
@@ -2,9 +2,13 @@ package com.demonstrate.SpringBootDemo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demonstrate.SpringBootDemo.dto.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -14,15 +18,20 @@ public class Controller {
 	public String sayHello() {
 		return "Hello from Bridgelabz";
 	}
-	
+
 	@GetMapping("/query")
 	public String sayHello(@RequestParam(value = "name") String name) {
 		return "Hello " + name + " from Bridgelabz";
 	}
-	
+
 	@GetMapping("/param/{name}")
 	public String sayHelloPath(@PathVariable String name) {
 		return "Hello " + name + " from Bridgelabz";
 	}
 	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " from Bridgelabz";
+	}
+
 }
